9aaefe3b044b63d248d96f86f4e86716821907c5
shiftinv <me@shiftinv.cc>
2022-05-04 23:39:55 +0000

    Send message source directly if length within message limit

--- backend/src/plugins/Utility/commands/SourceCmd.ts
+++ backend/src/plugins/Utility/commands/SourceCmd.ts
@@ -1,3 +1,4 @@
+import { escapeCodeBlock } from "discord.js";
 import moment from "moment-timezone";
 import { commandTypeHelpers as ct } from "../../../commandTypes";
 import { getBaseUrl, sendErrorMessage } from "../../../pluginUtils";
@@ -35,11 +36,18 @@ export const SourceCmd = utilityCmd({
       stickers: message.stickers,
     });
 
-    const source = `${textSource}\n\nSource:\n\n${fullSource}`;
+    const escaped = escapeCodeBlock(fullSource);
 
-    const archiveId = await pluginData.state.archives.create(source, moment.utc().add(1, "hour"));
-    const baseUrl = getBaseUrl(pluginData);
-    const url = pluginData.state.archives.getUrl(baseUrl, archiveId);
-    cmdMessage.channel.send(`Message source: ${url}`);
+    if (escaped.length < 1950) {
+      cmdMessage.channel.send("```json\n" + escaped + "\n```");
+    } else {
+      const archiveId = await pluginData.state.archives.create(
+        `${textSource}\n\nSource:\n\n${fullSource}`,
+        moment.utc().add(1, "hour"),
+      );
+      const baseUrl = getBaseUrl(pluginData);
+      const url = pluginData.state.archives.getUrl(baseUrl, archiveId);
+      cmdMessage.channel.send(`Message source: ${url}`);
+    }
   },
 });
